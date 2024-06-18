f4d6339e2defc6f37d99a30d14bff01616c786c7
anatg <anatg@starkware.co>
2024-06-18 11:07:34 +0300

    CR

--- crates/air_infra/src/airs/casm/common.rs
+++ crates/air_infra/src/airs/casm/common.rs
@@ -35,8 +35,8 @@ const OPCODE_ASSERT_EQ_BIT: u16 = 14;
 
 #[allow(clippy::too_many_arguments)]
 pub fn opcode_from_flags(
-    flag_dst_register: BoolExpr,
-    flag_op0_register: BoolExpr,
+    flag_dst_base_fp: BoolExpr,
+    flag_op0_base_fp: BoolExpr,
     flag_op1_imm: BoolExpr,
     flag_op1_base_fp: BoolExpr,
     flag_op1_base_ap: BoolExpr,
@@ -46,13 +46,13 @@ pub fn opcode_from_flags(
     flag_pc_update_jump_rel: BoolExpr,
     flag_pc_update_jnz: BoolExpr,
     flag_ap_update_add: BoolExpr,
-    flag_ap_update_add1: BoolExpr,
+    flag_ap_update_add_1: BoolExpr,
     flag_opcode_call: BoolExpr,
     flag_opcode_ret: BoolExpr,
     flag_opcode_assert_eq: BoolExpr,
 ) -> UInt16Expr {
-    &(&(&(&(&flag_dst_register.as_uint16() << &const_u16_expr!(DST_REG_BIT))
-        + &(&flag_op0_register.as_uint16() << &const_u16_expr!(OP0_REG_BIT)))
+    &(&(&(&(&flag_dst_base_fp.as_uint16() << &const_u16_expr!(DST_REG_BIT))
+        + &(&flag_op0_base_fp.as_uint16() << &const_u16_expr!(OP0_REG_BIT)))
         + &(&(&flag_op1_imm.as_uint16() << &const_u16_expr!(OP1_IMM_BIT))
             + &(&flag_op1_base_fp.as_uint16() << &const_u16_expr!(OP1_FP_BIT))))
         + &(&(&(&flag_op1_base_ap.as_uint16() << &const_u16_expr!(OP1_AP_BIT))
@@ -62,7 +62,7 @@ pub fn opcode_from_flags(
         + &(&(&(&(&flag_pc_update_jump_rel.as_uint16() << &const_u16_expr!(PC_JUMP_REL_BIT))
             + &(&flag_pc_update_jnz.as_uint16() << &const_u16_expr!(PC_JNZ_BIT)))
             + &(&(&flag_ap_update_add.as_uint16() << &const_u16_expr!(AP_ADD_BIT))
-                + &(&flag_ap_update_add1.as_uint16() << &const_u16_expr!(AP_ADD1_BIT))))
+                + &(&flag_ap_update_add_1.as_uint16() << &const_u16_expr!(AP_ADD1_BIT))))
             + &(&(&(&flag_opcode_call.as_uint16() << &const_u16_expr!(OPCODE_CALL_BIT))
                 + &(&flag_opcode_ret.as_uint16() << &const_u16_expr!(OPCODE_RET_BIT)))
                 + &(&flag_opcode_assert_eq.as_uint16() << &const_u16_expr!(OPCODE_ASSERT_EQ_BIT))))
@@ -73,4 +73,4 @@ pub fn offset_as_u16(offset: i16) -> UInt16Expr {
 }
 
 impl_air_var!([CasmAddress; 3]);
-impl_air_var!((CasmState, UInt16Expr, BoolExpr));
+impl_air_var!((CasmState, UInt16Expr));
