2109aa52a35b21a45e43b41afa473e4cf1e6cdd6
Stavbe <stav@starkware.co>
2024-08-08 13:00:56 +0300

    add write json files for opcodes using clap

--- crates/air_infra/src/main.rs
+++ crates/air_infra/src/main.rs
@@ -9,13 +9,8 @@ use air_infra::utils::fn_sizes::*;
 #[derive(Subcommand, Debug)]
 enum MainCommands {
     FnSizes,
-    WriteJsonExample {
-        fn_name: String,
-    }, // Usage: cargo run writre-json-example <function_name>
-    WriteJsonCasmOpcode {
-        fn_name: String,
-        opcode_args: Vec<bool>,
-    }, // Usage: cargo run write-json-casm-opcode <function_name> <arguments>
+    WriteJsonExample { fn_name: String }, // Usage: cargo run write-json-example <function_name>
+    WriteJsonCasmOpcode(CasmOpcodeCommand), // Usage: cargo run write-json-casm-opcode <opcode_name> --<flags set to true>
 }
 
 #[derive(Parser, Debug)]
@@ -41,44 +36,23 @@ pub fn write_json_example(fn_name: String) {
     }
 }
 
-pub fn write_json_casm_opcdode(fn_name: String, opcode_args: Vec<bool>) {
-    if fn_name == "assert_eq" {
-        assert!(
-            opcode_args.len() == 2,
-            "Expected 2 arguments for assert_eql: is_double_deref, is_immediate"
-        );
-        println!("Creating a json file for assert equal opcode");
-        create_assert_equal_opcdode_json(opcode_args);
-    } else if fn_name == "call" {
-        assert!(
-            opcode_args.len() == 2,
-            "Expected 2 arguments for call: is_rel, flag_op1_base_fp"
-        );
-        println!("Creating a json file for call opcode");
-        create_call_opcdode_json(opcode_args);
-    } else if fn_name == "jump" {
-        assert!(
-            opcode_args.len() == 3,
-            "Expected 3 arguments for jump: is_rel, flag_op1_base_fp, flag_ap_update_add_1"
-        );
-        println!("Creating a json file for jump opcode");
-        create_jump_opcdode_json(opcode_args);
-    } else if fn_name == "ret" {
-        assert!(opcode_args.is_empty(), "Expected 0 arguments for ret");
-        println!("Creating a json file for ret opcode");
-        create_ret_opcdode_json();
-    }
-}
-
 pub fn main() {
     let args = MainArgs::parse();
 
     match args.command {
         MainCommands::FnSizes => print_fn_sizes(),
         MainCommands::WriteJsonExample { fn_name } => write_json_example(fn_name),
-        MainCommands::WriteJsonCasmOpcode {
-            fn_name,
-            opcode_args,
-        } => write_json_casm_opcdode(fn_name, opcode_args),
+        MainCommands::WriteJsonCasmOpcode(opcode) => match opcode.command {
+            CasmOpcodeSubCommand::AssertEqual(assert_eq_opcode_args) => {
+                create_assert_equal_opcode_json(assert_eq_opcode_args)
+            }
+            CasmOpcodeSubCommand::Call(call_opcode_args) => {
+                create_call_opcode_json(call_opcode_args)
+            }
+            CasmOpcodeSubCommand::Jump(jump_opcode_args) => {
+                create_jump_opcode_json(jump_opcode_args)
+            }
+            CasmOpcodeSubCommand::Ret(_) => create_ret_opcode_json(),
+        },
     };
 }
--- crates/air_infra/src/utils/create_opcodes_json.rs
+++ crates/air_infra/src/utils/create_opcodes_json.rs
@@ -1,3 +1,6 @@
+use clap::Args;
+use clap::Subcommand;
+
 use crate::airs::casm::assert_eq_opcode::*;
 use crate::airs::casm::call_opcode::*;
 use crate::airs::casm::jump_opcode::*;
@@ -5,35 +8,91 @@ use crate::airs::casm::ret_opcode::*;
 use crate::core::air_fn_registry::*;
 use crate::core::memory::Memory;
 
-pub fn create_assert_equal_opcdode_json(arguments: Vec<bool>) {
+#[derive(Args, Debug)]
+pub struct CasmOpcodeCommand {
+    #[clap(subcommand)]
+    pub command: CasmOpcodeSubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum CasmOpcodeSubCommand {
+    AssertEqual(AssertEqOpcodeArgs),
+    Call(CallOpcodeArgs),
+    Jump(JumpOpcodeArgs),
+    Ret(RetOpcodeArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct AssertEqOpcodeArgs {
+    #[clap(long)]
+    is_double_deref: bool,
+    #[clap(long)]
+    is_immediate: bool,
+}
+
+#[derive(Debug, Args)]
+pub struct CallOpcodeArgs {
+    #[clap(long)]
+    is_rel: bool,
+    #[clap(long)]
+    flag_op1_base_fp: bool,
+}
+
+#[derive(Debug, Args)]
+pub struct JumpOpcodeArgs {
+    #[clap(long)]
+    is_rel: bool,
+    #[clap(long)]
+    flag_op1_base_fp: bool,
+    #[clap(long)]
+    flag_ap_update_add_1: bool,
+}
+
+#[derive(Debug, Args)]
+pub struct RetOpcodeArgs {}
+
+pub fn create_assert_equal_opcode_json(arguments: AssertEqOpcodeArgs) {
+    println!(
+        "Creating a json file for assert_equal_opcode with arguments: {:?}",
+        arguments
+    );
     let registry = AirFnRegistry::new(&AssertEqOpcode {
-        is_double_deref: arguments[0],
-        is_immediate: arguments[1],
+        is_double_deref: arguments.is_double_deref,
+        is_immediate: arguments.is_immediate,
         memory: Memory::default(),
     });
     registry.dump_to_file("airs/casm/assert_equal_opcode.json");
 }
 
-pub fn create_call_opcdode_json(arguments: Vec<bool>) {
+pub fn create_call_opcode_json(arguments: CallOpcodeArgs) {
+    println!(
+        "Creating a json file for call_opcode with arguments: {:?}",
+        arguments
+    );
     let registry = AirFnRegistry::new(&CallOpcode {
-        is_rel: arguments[0],
-        flag_op1_base_fp: arguments[1],
+        is_rel: arguments.is_rel,
+        flag_op1_base_fp: arguments.flag_op1_base_fp,
         memory: Memory::default(),
     });
     registry.dump_to_file("airs/casm/call_opcode.json");
 }
 
-pub fn create_jump_opcdode_json(arguments: Vec<bool>) {
+pub fn create_jump_opcode_json(arguments: JumpOpcodeArgs) {
+    println!(
+        "Creating a json file for jump_opcode with arguments: {:?}",
+        arguments
+    );
     let registry = AirFnRegistry::new(&JumpOpcode {
-        is_rel: arguments[0],
-        flag_op1_base_fp: arguments[1],
-        flag_ap_update_add_1: arguments[2],
+        is_rel: arguments.is_rel,
+        flag_op1_base_fp: arguments.flag_op1_base_fp,
+        flag_ap_update_add_1: arguments.flag_ap_update_add_1,
         memory: Memory::default(),
     });
     registry.dump_to_file("airs/casm/jump_opcode.json");
 }
 
-pub fn create_ret_opcdode_json() {
+pub fn create_ret_opcode_json() {
+    println!("Creating a json file for ret_opcode");
     let registry = AirFnRegistry::new(&RetOpcode {
         memory: Memory::default(),
     });
