edd92458e160d80bcd609e57010c2b8b62d663c3
Ohad Nir <141617878+ohad-nir-starkware@users.noreply.github.com>
2024-08-12 16:01:38 +0300

    Ohadn/add small opcode (#170)

---
+++ crates/air_infra/src/airs/casm/add_small_opcode.rs
@@ -0,0 +1,168 @@
+use indexmap::IndexMap;
+
+use crate::core::air_fn::*;
+use crate::core::expressions::felt252_expr::*;
+use crate::core::expressions::felt_expr::*;
+use crate::core::memory::*;
+use crate::core::prover_types::*;
+
+use super::check_instruction::*;
+use super::common::*;
+use super::read_small_felt252::*;
+
+// Macros
+use crate::const_expr;
+
+/// The add_small opcode.
+/// Implements the Cairo0 instructions:
+/// - [ap/fp + offset0] = [ap/fp + offset1] + [ap/fp + offset2]
+/// - [ap/fp + offset0] = [ap/fp + offset1] + Imm
+/// Where all three values are in the range [0, 2^27-1].
+///
+/// TODO: Update the range when the correct range is known.
+
+#[derive(Clone, Debug)]
+pub struct AddSmallOpcode {
+    pub is_immediate: bool,
+    pub memory: Memory<FeltExpr, Felt252Expr>,
+}
+
+impl AddSmallOpcode {
+    pub fn get_flags(&self) -> Flags {
+        Flags {
+            dst_base_fp: None,
+            op0_base_fp: None,
+            op1_imm: Some(self.is_immediate),
+            op1_base_fp: if !self.is_immediate {
+                None
+            } else {
+                Some(false)
+            },
+            op1_base_ap: if !self.is_immediate {
+                None
+            } else {
+                Some(false)
+            },
+            res_add: Some(true),
+            res_mul: Some(false),
+            pc_update_jump: Some(false),
+            pc_update_jump_rel: Some(false),
+            pc_update_jnz: Some(false),
+            ap_update_add: Some(false),
+            ap_update_add_1: None,
+            opcode_call: Some(false),
+            opcode_ret: Some(false),
+            opcode_assert_eq: Some(true),
+        }
+    }
+}
+
+impl AirFn for AddSmallOpcode {
+    type In = CasmState;
+    type Out = CasmState;
+
+    fn call(&self, ab: &mut AirBuilder, [pc, ap, fp]: Self::In) -> Self::Out {
+        let const_offsets = if self.is_immediate {
+            [None, None, Some(1)]
+        } else {
+            [None, None, None]
+        };
+        // Check the instruction.
+        let ([offset0, offset1, offset2], flags) = ab.call(
+            &CheckInstruction {
+                const_offsets,
+                const_flags: self.get_flags(),
+                memory: self.memory.clone(),
+            },
+            pc.clone(),
+        );
+
+        // Read the non-constant flags
+        let flag_dst_base_fp = flags[FLAG_DST_BASE_FP].as_felt();
+        let flag_op0_base_fp = flags[FLAG_OP0_BASE_FP].as_felt();
+        let flag_op1_base_fp = flags[FLAG_OP1_BASE_FP].as_felt();
+        let flag_op1_base_ap = flags[FLAG_OP1_BASE_AP].as_felt();
+        let flag_ap_update_add_1 = flags[FLAG_AP_UPDATE_ADD_1].as_felt();
+
+        // Fetch dst - the value at the destination address for the addition
+        let mem_dst_base = flag_dst_base_fp.clone() * fp.clone()
+            + (const_expr!(1) - flag_dst_base_fp) * ap.clone();
+        let dst_value = ab.call(
+            &ReadSmallFelt252 {
+                num_bits: 3 * FELT252_BITS_PER_WORD,
+                memory: self.memory.clone(),
+            },
+            mem_dst_base + offset0,
+        );
+        let dst_m31 = dst_value.get_felt(0).clone()
+            + const_expr!(1 << FELT252_BITS_PER_WORD) * dst_value.get_felt(1).clone()
+            + const_expr!(1 << (2 * FELT252_BITS_PER_WORD)) * dst_value.get_felt(2).clone();
+
+        // Fetch op0 - the first operand for the addition
+        let mem0_base = flag_op0_base_fp.clone() * fp.clone()
+            + (const_expr!(1) - flag_op0_base_fp) * ap.clone();
+        let op0_value = ab.call(
+            &ReadSmallFelt252 {
+                num_bits: 3 * FELT252_BITS_PER_WORD,
+                memory: self.memory.clone(),
+            },
+            mem0_base + offset1,
+        );
+        let op0_m31 = op0_value.get_felt(0).clone()
+            + const_expr!(1 << FELT252_BITS_PER_WORD) * op0_value.get_felt(1).clone()
+            + const_expr!(1 << (2 * FELT252_BITS_PER_WORD)) * op0_value.get_felt(2).clone();
+
+        // Fetch op1 - the second operand for the addition
+        let mem1_base = if self.is_immediate {
+            pc.clone()
+        } else {
+            ab.constrain(flag_op1_base_fp.clone() + flag_op1_base_ap.clone() - const_expr!(1));
+            flag_op1_base_fp * fp.clone() + flag_op1_base_ap * ap.clone()
+        };
+        let op1_value = ab.call(
+            &ReadSmallFelt252 {
+                num_bits: 3 * FELT252_BITS_PER_WORD,
+                memory: self.memory.clone(),
+            },
+            mem1_base + offset2,
+        );
+        let op1_m31 = op1_value.get_felt(0).clone()
+            + const_expr!(1 << FELT252_BITS_PER_WORD) * op1_value.get_felt(1).clone()
+            + const_expr!(1 << (2 * FELT252_BITS_PER_WORD)) * op1_value.get_felt(2).clone();
+
+        let res = op0_m31 + op1_m31;
+
+        // Assert that dst == res
+        ab.constrain(dst_m31 - res);
+
+        // Calculate the next ap
+        let next_ap = (const_expr!(1) - flag_ap_update_add_1.clone()) * ap.clone()
+            + flag_ap_update_add_1 * (ap + const_expr!(1));
+
+        // Calculate the next pc
+        let next_pc = if self.is_immediate {
+            pc + const_expr!(2)
+        } else {
+            pc + const_expr!(1)
+        };
+
+        [next_pc, next_ap, fp]
+    }
+
+    fn inst_def(&self) -> IndexMap<String, String> {
+        [("is_immediate".to_string(), self.is_immediate.to_string())].into()
+    }
+
+    fn trace_type(&self) -> TraceType {
+        TraceType::Component
+    }
+}
+
+impl MemoryAirFn for AddSmallOpcode {
+    type K = FeltExpr;
+    type V = Felt252Expr;
+
+    fn init_memory(&mut self, memory: &Memory<FeltExpr, Felt252Expr>) {
+        self.memory = memory.clone();
+    }
+}
---
+++ crates/air_infra/src/airs/casm/add_small_opcode_test.rs
@@ -0,0 +1,360 @@
+use super::add_small_opcode::*;
+use super::common::*;
+
+use crate::core::air_fn::*;
+use crate::core::air_fn_registry::*;
+use crate::core::expressions::expr::*;
+use crate::core::expressions::felt252_expr::*;
+use crate::core::expressions::felt_expr::*;
+use crate::core::memory::*;
+
+// Macros
+use crate::const_expr;
+use crate::expr;
+use crate::felt252_expr;
+
+fn test_add_small(
+    non_consts_flags: [bool; 6],
+    offset_values: [i16; 3],
+    dst: u32,
+    op0: u32,
+    op1: u32,
+    check_instruction_body: Option<(&str, &[&str], &str)>,
+    memory_body: Option<&[&str]>,
+) {
+    // Read the non-constant flags
+    let [flag_dst_base_fp, flag_op0_base_fp, flag_op1_imm, flag_op1_base_fp, flag_op1_base_ap, flag_ap_update_add_1] =
+        non_consts_flags;
+
+    let [offset_dst_val, offset0_val, mut offset1_val] = offset_values;
+    if flag_op1_imm {
+        offset1_val = 1;
+    }
+
+    // Create the air function
+    let mut add_small_opcode = AddSmallOpcode {
+        is_immediate: flag_op1_imm,
+        memory: Memory::default(),
+    };
+
+    // Register values at opcode start
+    let pc_value = 10;
+    let ap_value = 50;
+    let fp_value = 100;
+
+    let pc = expr!("pc", pc_value);
+    let ap = expr!("ap", ap_value);
+    let fp = expr!("fp", fp_value);
+    let offset_dst = offset_as_u16(offset_dst_val);
+    let offset0 = offset_as_u16(offset0_val);
+    let offset1 = offset_as_u16(offset1_val);
+
+    // Cretae the non-constant flags
+    let non_consts_flags = if flag_op1_imm {
+        vec![flag_dst_base_fp, flag_op0_base_fp, flag_ap_update_add_1]
+    } else {
+        vec![
+            flag_dst_base_fp,
+            flag_op0_base_fp,
+            flag_op1_base_fp,
+            flag_op1_base_ap,
+            flag_ap_update_add_1,
+        ]
+    };
+
+    // Fill memory
+    let mut memory_values = vec![(
+        pc.clone(),
+        felt252_expr!(
+            "op",
+            assemble_instruction(
+                offset_dst_val,
+                offset0_val,
+                offset1_val,
+                add_small_opcode
+                    .get_flags()
+                    .non_constants_to_arr(non_consts_flags),
+            ) as u128,
+            0
+        ),
+    )];
+    if flag_dst_base_fp {
+        memory_values.push((
+            const_expr!((fp_value as i16 + offset_dst_val) as u32),
+            felt252_expr!("dst", dst as u128, 0),
+        ));
+    } else {
+        memory_values.push((
+            const_expr!((ap_value as i16 + offset_dst_val) as u32),
+            felt252_expr!("dst", dst as u128, 0),
+        ));
+    };
+    if flag_op0_base_fp {
+        memory_values.push((
+            const_expr!((fp_value as i16 + offset0_val) as u32),
+            felt252_expr!("op0", op0 as u128, 0),
+        ));
+    } else {
+        memory_values.push((
+            const_expr!((ap_value as i16 + offset0_val) as u32),
+            felt252_expr!("op0", op0 as u128, 0),
+        ));
+    }
+    if flag_op1_imm {
+        memory_values.push((
+            const_expr!(pc_value + 1),
+            felt252_expr!("op1", op1 as u128, 0),
+        ));
+    } else if flag_op1_base_fp {
+        memory_values.push((
+            const_expr!((fp_value as i16 + offset1_val) as u32),
+            felt252_expr!("op1", op1 as u128, 0),
+        ));
+    } else {
+        memory_values.push((
+            const_expr!((ap_value as i16 + offset1_val) as u32),
+            felt252_expr!("op1", op1 as u128, 0),
+        ));
+    };
+    add_small_opcode.init_memory(&Memory::new_with_data(memory_values));
+
+    // Run air function
+
+    let registry = AirFnRegistry::new(&add_small_opcode);
+    let (state, [next_pc, next_ap, next_fp]) =
+        registry.run_air(&add_small_opcode, [pc.clone(), ap.clone(), fp.clone()]);
+
+    // Check output
+    assert_eq!(next_fp.calc(), fp.calc());
+    if flag_ap_update_add_1 {
+        assert_eq!(next_ap.calc(), (ap_value + 1).to_string());
+    } else {
+        assert_eq!(next_ap.calc(), ap.calc());
+    }
+    if flag_op1_imm {
+        assert_eq!(next_pc.calc(), (pc_value + 2).to_string());
+    } else {
+        assert_eq!(next_pc.calc(), (pc_value + 1).to_string());
+    };
+
+    // Check state
+    let mut expected_state = vec![
+        pc_value,
+        ap_value,
+        fp_value,
+        (offset_dst & 0x1FF) as u32,
+        (offset_dst >> 9) as u32,
+        (offset0 & 0x3) as u32,
+        ((offset0 >> 2) & 0x1FF) as u32,
+        (offset0 >> 11) as u32,
+    ];
+    if !flag_op1_imm {
+        expected_state.push((offset1 & 0xF) as u32);
+        expected_state.push(((offset1 >> 4) & 0x1FF) as u32);
+        expected_state.push((offset1 >> 13) as u32);
+    };
+    expected_state.push(flag_dst_base_fp as u32);
+    expected_state.push(flag_op0_base_fp as u32);
+    if !flag_op1_imm {
+        expected_state.push(flag_op1_base_fp as u32);
+        expected_state.push(flag_op1_base_ap as u32);
+    }
+    expected_state.push(flag_ap_update_add_1 as u32);
+
+    expected_state.push(dst & 0x1FF);
+    expected_state.push((dst >> 9) & 0x1FF);
+    expected_state.push((dst >> 18) & 0x1FF);
+
+    expected_state.push(op0 & 0x1FF);
+    expected_state.push((op0 >> 9) & 0x1FF);
+    expected_state.push((op0 >> 18) & 0x1FF);
+
+    expected_state.push(op1 & 0x1FF);
+    expected_state.push((op1 >> 9) & 0x1FF);
+    expected_state.push((op1 >> 18) & 0x1FF);
+
+    assert_eq!(
+        state.calc(),
+        expected_state
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+    );
+
+    // Check air body
+    if let Some((check_instruction_offsets, check_instruction_flags, check_instruction_name)) =
+        check_instruction_body
+    {
+        let memory_body_entries = memory_body.unwrap();
+        let entry = registry.get_air_fn_entry(&add_small_opcode);
+        let air_body = [
+            &format!(
+                "deduction_tmp_0 = [{name}_input[0], {name}_input[1], {name}_input[2]]",
+                name = add_small_opcode.name()
+            ),
+            "Deduction: deduction_tmp_0[0]",
+            "Deduction: deduction_tmp_0[1]",
+            "Deduction: deduction_tmp_0[2]",
+            &format!(
+                "({}, [{}]) = {}({})",
+                check_instruction_offsets,
+                add_small_opcode
+                    .get_flags()
+                    .to_string(check_instruction_flags.to_vec()),
+                check_instruction_name,
+                "state[0]"
+            ),
+        ];
+        let mut air_body_vec = air_body.to_vec();
+        air_body_vec.append(&mut memory_body_entries.to_vec());
+
+        assert_eq!(
+            entry
+                .air_body
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>(),
+            air_body_vec
+        );
+    }
+}
+
+#[test]
+fn test_add_small_not_imm() {
+    let check_instruction_offsets = &format!(
+        "[{}, {}, {}]",
+        "(((state[3] + (state[4] * const_512)) + const_0) - const_32768)",
+        "(((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768)",
+        "(((state[8] + (state[9] * const_16)) + (state[10] * const_8192)) - const_32768)"
+    );
+    let check_instruction_flags = vec![
+        "Bool::from_m31(state[11])",
+        "Bool::from_m31(state[12])",
+        "Bool::from_m31(state[13])",
+        "Bool::from_m31(state[14])",
+        "Bool::from_m31(state[15])",
+    ];
+    let memory_body: [&str; 5] = [
+        &format!(
+            "{} = {}({})",
+            "Felt252::from_m31_(zero_extend([state[16], state[17], state[18]]))",
+            "ReadSmallFelt252_90f301596f69aa5a",
+            "(((state[11] * state[2]) + ((const_1 - state[11]) * state[1])) + \
+                (((state[3] + (state[4] * const_512)) + const_0) - const_32768))"
+        ),
+        &format!(
+            "{} = {}({})",
+            "Felt252::from_m31_(zero_extend([state[19], state[20], state[21]]))",
+            "ReadSmallFelt252_90f301596f69aa5a",
+            "(((state[12] * state[2]) + ((const_1 - state[12]) * state[1])) + \
+                (((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768))"
+        ),
+        "Constraint: ((state[13] + state[14]) - const_1)",
+        &format!(
+            "{} = {}({})",
+            "Felt252::from_m31_(zero_extend([state[22], state[23], state[24]]))",
+            "ReadSmallFelt252_90f301596f69aa5a",
+            "(((state[13] * state[2]) + (state[14] * state[1])) + \
+                (((state[8] + (state[9] * const_16)) + (state[10] * const_8192)) - const_32768))"
+        ),
+        "Constraint: (((state[16] + (const_512 * state[17])) + (const_262144 * state[18])) - \
+            (((state[19] + (const_512 * state[20])) + (const_262144 * state[21])) + \
+            ((state[22] + (const_512 * state[23])) + (const_262144 * state[24]))))",
+    ];
+
+    test_add_small(
+        [true, false, false, false, true, false],
+        [3, 5, 7],
+        90125677,
+        77779999,
+        12345678,
+        Some((
+            check_instruction_offsets,
+            &check_instruction_flags,
+            "CheckInstruction_66e58212c867a935",
+        )),
+        Some(&memory_body),
+    );
+}
+
+#[test]
+#[should_panic]
+fn test_add_mod_not_equal() {
+    test_add_small(
+        [false, true, true, false, false, true],
+        [3, 5, 7],
+        90124653,
+        77779999,
+        12345678,
+        None,
+        None,
+    );
+}
+
+#[test]
+#[should_panic]
+fn test_add_mod_over_27bit() {
+    test_add_small(
+        [false, true, true, false, false, true],
+        [3, 5, 7],
+        134217728,
+        134217727,
+        1,
+        None,
+        None,
+    );
+}
+
+#[test]
+fn test_add_small_imm() {
+    let check_instruction_offsets = &format!(
+        "[{}, {}, {}]",
+        "(((state[3] + (state[4] * const_512)) + const_0) - const_32768)",
+        "(((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768)",
+        "const_1"
+    );
+    let check_instruction_flags = vec![
+        "Bool::from_m31(state[8])",
+        "Bool::from_m31(state[9])",
+        "Bool::from_m31(state[10])",
+    ];
+    let memory_body: [&str; 4] = [
+        &format!(
+            "{} = {}({})",
+            "Felt252::from_m31_(zero_extend([state[11], state[12], state[13]]))",
+            "ReadSmallFelt252_90f301596f69aa5a",
+            "(((state[8] * state[2]) + ((const_1 - state[8]) * state[1])) + \
+                (((state[3] + (state[4] * const_512)) + const_0) - const_32768))"
+        ),
+        &format!(
+            "{} = {}({})",
+            "Felt252::from_m31_(zero_extend([state[14], state[15], state[16]]))",
+            "ReadSmallFelt252_90f301596f69aa5a",
+            "(((state[9] * state[2]) + ((const_1 - state[9]) * state[1])) + \
+                (((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768))"
+        ),
+        &format!(
+            "{} = {}({})",
+            "Felt252::from_m31_(zero_extend([state[17], state[18], state[19]]))",
+            "ReadSmallFelt252_90f301596f69aa5a",
+            "(state[0] + const_1)"
+        ),
+        "Constraint: (((state[11] + (const_512 * state[12])) + (const_262144 * state[13])) - \
+            (((state[14] + (const_512 * state[15])) + (const_262144 * state[16])) + \
+            ((state[17] + (const_512 * state[18])) + (const_262144 * state[19]))))",
+    ];
+
+    test_add_small(
+        [true, false, true, false, true, false],
+        [-3, -5, 1],
+        90125677,
+        77779999,
+        12345678,
+        Some((
+            check_instruction_offsets,
+            &check_instruction_flags,
+            "CheckInstruction_d01b0a2c2642087a",
+        )),
+        Some(&memory_body),
+    );
+}
