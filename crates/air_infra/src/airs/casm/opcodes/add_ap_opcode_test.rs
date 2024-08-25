c3dba4c51b8553a38df4807b9e078e5cf10a3820
Stavbe <stav@starkware.co>
2024-08-25 18:37:01 +0300

    add support to ap += [fp/ap+off]

--- crates/air_infra/src/airs/casm/opcodes/add_ap_opcode_test.rs
+++ crates/air_infra/src/airs/casm/opcodes/add_ap_opcode_test.rs
@@ -13,53 +13,57 @@ use crate::utils::test_utils::*;
 // Macros
 use crate::const_expr;
 use crate::const_felt252_expr;
-use crate::expr;
 
 #[test]
-fn test_add_ap() {
+fn test_add_ap_negative_imm() {
     // build the air function
     let mut add_ap_opcode = AddAp {
+        is_imm: true,
+        op1_base_fp: false,
         memory: Felt252IdMemory::default(),
     };
 
     // Register values at opcode start
-    let pc_value = 30;
-    let ap_value = 11;
-    let fp_value = 6;
-    let immediate = 299;
-
-    let pc = expr!("pc", pc_value);
-    let ap = expr!("ap", ap_value);
-    let fp = expr!("fp", fp_value);
+    let pc = 30;
+    let ap = 11;
+    let fp = 6;
 
     // Fill memory
     let mut memory_values = vec![(
-        pc.clone(),
+        const_expr!(pc),
         const_felt252_expr!(
             assemble_instruction(-1, -1, 1, add_ap_opcode.get_flags().into()) as u128,
             0
         ),
     )];
-    memory_values.push((const_expr!(pc_value + 1), const_felt252_expr!(immediate, 0)));
+    memory_values.push((const_expr!(pc + 1), const_felt252_expr!(-1i128)));
     add_ap_opcode.memory = Felt252IdMemory::new_with_data(memory_values);
 
     // Run air function
     let registry = AirFnRegistry::new(&add_ap_opcode);
     let (state, next_state) = registry.run_air(
         &add_ap_opcode,
-        CasmStateVar::new(pc, ap.clone(), fp.clone()),
+        CasmStateVar::new(const_expr!(pc), const_expr!(ap), const_expr!(fp)),
     );
 
     // Check the output
-    assert_eq!(next_state.pc.calc(), (pc_value + 2).to_string());
-    assert_eq!(next_state.fp.calc(), (fp_value).to_string());
-    assert_eq!(
-        next_state.ap.calc(),
-        (ap_value + immediate as u32).to_string()
-    );
+    assert_eq!(next_state.pc.calc(), (pc + 2).to_string());
+    assert_eq!(next_state.fp.calc(), (fp).to_string());
+    assert_eq!(next_state.ap.calc(), (ap - 1).to_string());
 
     // Check the state
-    let expected_state = [30, 11, 6, 0, 1, 0, 0, 299, 0, 0];
+    let expected_state = [
+        "30", // pc
+        "11", // ap
+        "6",  // fp
+        "0",  // instruction id
+        "1",  // op1 id
+        "1",  // op1 (sign)
+        "0",  // op1 (sign)
+        "0",  // op1
+        "0",  // op1
+        "0",  // op1
+    ];
     assert_eq!(
         state.calc(),
         expected_state
@@ -72,6 +76,113 @@ fn test_add_ap() {
     compare_test_json(
         registry,
         &add_ap_opcode.name(),
-        &(TEST_JSONS_OPCODES_DIR.to_owned() + "add_ap.json"),
+        &(TEST_JSONS_OPCODES_DIR.to_owned() + "add_ap_negative_imm.json"),
+    );
+}
+
+#[test]
+fn test_add_ap_deref_base_fp() {
+    // build the air function
+    let mut add_ap_opcode = AddAp {
+        is_imm: false,
+        op1_base_fp: true,
+        memory: Felt252IdMemory::default(),
+    };
+
+    // Register values at opcode start
+    let pc = 30;
+    let ap = 11;
+    let fp = 6;
+    let op1 = 299;
+    let offset2 = 400;
+
+    // Fill memory
+    let mut memory_values = vec![(
+        const_expr!(pc),
+        const_felt252_expr!(
+            assemble_instruction(-1, -1, offset2, add_ap_opcode.get_flags().into()) as u128,
+            0
+        ),
+    )];
+    memory_values.push((
+        const_expr!((fp as i16 + offset2) as u32),
+        const_felt252_expr!(op1),
+    ));
+    add_ap_opcode.memory = Felt252IdMemory::new_with_data(memory_values);
+
+    // Run air function
+    let registry = AirFnRegistry::new(&add_ap_opcode);
+    let (state, next_state) = registry.run_air(
+        &add_ap_opcode,
+        CasmStateVar::new(const_expr!(pc), const_expr!(ap), const_expr!(fp)),
+    );
+
+    // Check the output
+    assert_eq!(next_state.pc.calc(), (pc + 2).to_string());
+    assert_eq!(next_state.fp.calc(), (fp).to_string());
+    assert_eq!(next_state.ap.calc(), (ap + op1 as u32).to_string());
+
+    // Check the state
+    let expected_state = [
+        "30",  // pc
+        "11",  // ap
+        "6",   // fp
+        "0",   // offset2
+        "25",  // offset2
+        "4",   // offset2
+        "0",   // instruction id
+        "1",   // op1_id
+        "0",   // op1 (sign)
+        "0",   // op1(sign)
+        "299", // op1
+        "0",   // op1
+        "0",   // op1
+    ];
+    assert_eq!(state.calc(), expected_state);
+
+    // Check entry
+    compare_test_json(
+        registry,
+        &add_ap_opcode.name(),
+        &(TEST_JSONS_OPCODES_DIR.to_owned() + "add_ap_deref_base_fp.json"),
+    );
+}
+
+#[test]
+#[should_panic(expected = "FLAG_OP1_IMM and FLAG_OP1_BASE_FP cannot be set at the same time.")]
+fn test_failed_op1_src() {
+    // build the air function
+    let mut add_ap_opcode = AddAp {
+        is_imm: true,
+        op1_base_fp: true,
+        memory: Felt252IdMemory::default(),
+    };
+
+    // Register values at opcode start
+    let pc = 30;
+    let ap = 11;
+    let fp = 6;
+    let op1 = 299;
+    let offset2 = 400;
+
+    // Fill memory
+    let mut memory_values = vec![(
+        const_expr!(pc),
+        const_felt252_expr!(
+            assemble_instruction(-1, -1, offset2, add_ap_opcode.get_flags().into()) as u128,
+            0
+        ),
+    )];
+    memory_values.push((
+        const_expr!((fp as i16 + offset2) as u32),
+        const_felt252_expr!(op1),
+    ));
+    add_ap_opcode.memory = Felt252IdMemory::new_with_data(memory_values);
+
+    // Run air function
+    let registry = AirFnRegistry::new(&add_ap_opcode);
+    registry.run_air(
+        &add_ap_opcode,
+        CasmStateVar::new(const_expr!(pc), const_expr!(ap), const_expr!(fp)),
     );
 }
