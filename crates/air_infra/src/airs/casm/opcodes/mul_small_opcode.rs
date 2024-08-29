6347b3fdffe89887b27cf253c1ee2086276ed7fd
Ohad Nir <141617878+ohad-nir-starkware@users.noreply.github.com>
2024-08-29 16:06:42 +0300

    Ohadn/mul small opcode (#176)

---
+++ crates/air_infra/src/airs/casm/opcodes/mul_small_opcode.rs
@@ -0,0 +1,157 @@
+use indexmap::IndexMap;
+
+use crate::airs::memory::felt252_id_memory::*;
+use crate::airs::memory::felt252_id_memory_read_positive::*;
+use crate::core::air_fn::*;
+use crate::core::expressions::felt_expr::*;
+use crate::core::prover_types::*;
+
+use super::super::common::*;
+use super::decode_instruction::*;
+
+// Macros
+use crate::const_expr;
+
+/// The mul_small opcode.
+/// Implements the Cairo0 instructions:
+/// - [ap/fp + offset0] = [ap/fp + offset1] * [ap/fp + offset2]
+/// - [ap/fp + offset0] = [ap/fp + offset1] * Imm
+/// Where multiplication factors are in the range [0, 2^15-1].
+///
+/// TODO: Support negatives and update the range when the correct range is known.
+
+#[derive(Clone, Debug)]
+pub struct MulSmallOpcode {
+    pub is_immediate: bool,
+    pub memory: Felt252IdMemory,
+}
+
+impl MulSmallOpcode {
+    pub fn get_flags(&self) -> Flags {
+        Flags {
+            dst_base_fp: None,
+            op0_base_fp: None,
+            op1_imm: Some(self.is_immediate),
+            op1_base_fp: if !self.is_immediate {
+                None
+            } else {
+                Some(false)
+            },
+            op1_base_ap: if !self.is_immediate {
+                None
+            } else {
+                Some(false)
+            },
+            res_add: Some(false),
+            res_mul: Some(true),
+            pc_update_jump: Some(false),
+            pc_update_jump_rel: Some(false),
+            pc_update_jnz: Some(false),
+            ap_update_add: Some(false),
+            ap_update_add_1: None,
+            opcode_call: Some(false),
+            opcode_ret: Some(false),
+            opcode_assert_eq: Some(true),
+        }
+    }
+}
+
+impl AirFn for MulSmallOpcode {
+    type In = CasmState;
+    type Out = CasmState;
+
+    fn call(&self, ab: &mut AirBuilder, [pc, ap, fp]: Self::In) -> Self::Out {
+        let const_offsets = if self.is_immediate {
+            [None, None, Some(1)]
+        } else {
+            [None, None, None]
+        };
+        // Check the instruction.
+        let ([offset0, offset1, offset2], flags) = ab.call(
+            &DecodeInstruction {
+                const_offsets,
+                const_flags: self.get_flags(),
+                memory: self.memory.clone(),
+            },
+            pc.clone(),
+        );
+
+        // Read the non-constant flags
+        let flag_dst_base_fp = flags[FLAG_DST_BASE_FP].as_felt();
+        let flag_op0_base_fp = flags[FLAG_OP0_BASE_FP].as_felt();
+        let flag_op1_base_fp = flags[FLAG_OP1_BASE_FP].as_felt();
+        let flag_op1_base_ap = flags[FLAG_OP1_BASE_AP].as_felt();
+        let flag_ap_update_add_1 = flags[FLAG_AP_UPDATE_ADD_1].as_felt();
+
+        // Fetch dst - the value at the destination address for the multiplication
+        let mem_dst_base = flag_dst_base_fp.clone() * fp.clone()
+            + (const_expr!(1) - flag_dst_base_fp) * ap.clone();
+        let dst_value = ab.call(
+            &ReadPositive {
+                num_bits: 30,
+                memory: self.memory.clone(),
+            },
+            mem_dst_base + offset0,
+        );
+        let dst_m31 = dst_value.get_felt(0).clone()
+            + const_expr!(1 << FELT252_BITS_PER_WORD) * dst_value.get_felt(1).clone()
+            + const_expr!(1 << (2 * FELT252_BITS_PER_WORD)) * dst_value.get_felt(2).clone()
+            + const_expr!(1 << (3 * FELT252_BITS_PER_WORD)) * dst_value.get_felt(3).clone();
+
+        // Fetch op0 - the first operand for the multiplication
+        let mem0_base = flag_op0_base_fp.clone() * fp.clone()
+            + (const_expr!(1) - flag_op0_base_fp) * ap.clone();
+        let op0_value = ab.call(
+            &ReadPositive {
+                num_bits: 15,
+                memory: self.memory.clone(),
+            },
+            mem0_base + offset1,
+        );
+        let op0_m31 = op0_value.get_felt(0).clone()
+            + const_expr!(1 << FELT252_BITS_PER_WORD) * op0_value.get_felt(1).clone();
+
+        // Fetch op1 - the second operand for the multiplication
+        let mem1_base = if self.is_immediate {
+            pc.clone()
+        } else {
+            ab.constrain(flag_op1_base_fp.clone() + flag_op1_base_ap.clone() - const_expr!(1));
+            flag_op1_base_fp * fp.clone() + flag_op1_base_ap * ap.clone()
+        };
+        let op1_value = ab.call(
+            &ReadPositive {
+                num_bits: 15,
+                memory: self.memory.clone(),
+            },
+            mem1_base + offset2,
+        );
+        let op1_m31 = op1_value.get_felt(0).clone()
+            + const_expr!(1 << FELT252_BITS_PER_WORD) * op1_value.get_felt(1).clone();
+
+        let res = op0_m31 * op1_m31;
+
+        // Assert that dst == res
+        ab.constrain(dst_m31 - res);
+
+        // Calculate the next ap
+        let next_ap = (const_expr!(1) - flag_ap_update_add_1.clone()) * ap.clone()
+            + flag_ap_update_add_1 * (ap + const_expr!(1));
+
+        // Calculate the next pc
+        let next_pc = if self.is_immediate {
+            pc + const_expr!(2)
+        } else {
+            pc + const_expr!(1)
+        };
+
+        [next_pc, next_ap, fp]
+    }
+
+    fn inst_def(&self) -> IndexMap<String, String> {
+        [("is_immediate".to_string(), self.is_immediate.to_string())].into()
+    }
+
+    fn trace_type(&self) -> TraceType {
+        TraceType::Component
+    }
+}
---
+++ crates/air_infra/src/airs/casm/opcodes/mul_small_opcode_test.rs
@@ -0,0 +1,307 @@
+use super::super::common::*;
+use super::mul_small_opcode::*;
+
+use crate::airs::memory::felt252_id_memory::*;
+use crate::core::air_fn::*;
+use crate::core::air_fn_registry::*;
+use crate::core::expressions::felt252_expr::*;
+use crate::core::expressions::felt_expr::*;
+use crate::core::variables::*;
+
+// Macros
+use crate::const_expr;
+use crate::expr;
+use crate::felt252_expr;
+
+fn test_mul_small(
+    non_consts_flags: [bool; 6],
+    offset_values: [i16; 3],
+    dst: u32,
+    op0: u32,
+    op1: u32,
+    expected_air_body: Option<&[&str]>,
+    expected_state: Vec<u32>,
+) {
+    // Read the non-constant flags
+    let [flag_dst_base_fp, flag_op0_base_fp, flag_op1_imm, flag_op1_base_fp, flag_op1_base_ap, flag_ap_update_add_1] =
+        non_consts_flags;
+
+    let [offset_dst_val, offset0_val, mut offset1_val] = offset_values;
+    if flag_op1_imm {
+        offset1_val = 1;
+    }
+
+    // Create the air function
+    let mut mul_small_opcode = MulSmallOpcode {
+        is_immediate: flag_op1_imm,
+        memory: Felt252IdMemory::default(),
+    };
+
+    // Register values at opcode start
+    let pc_value = 10;
+    let ap_value = 50;
+    let fp_value = 100;
+
+    let pc = expr!("pc", pc_value);
+    let ap = expr!("ap", ap_value);
+    let fp = expr!("fp", fp_value);
+
+    // Cretae the non-constant flags
+    let non_consts_flags = if flag_op1_imm {
+        vec![flag_dst_base_fp, flag_op0_base_fp, flag_ap_update_add_1]
+    } else {
+        vec![
+            flag_dst_base_fp,
+            flag_op0_base_fp,
+            flag_op1_base_fp,
+            flag_op1_base_ap,
+            flag_ap_update_add_1,
+        ]
+    };
+
+    // Fill memory
+    let mut memory_values = vec![(
+        pc.clone(),
+        felt252_expr!(
+            "op",
+            assemble_instruction(
+                offset_dst_val,
+                offset0_val,
+                offset1_val,
+                mul_small_opcode
+                    .get_flags()
+                    .non_constants_to_arr(non_consts_flags),
+            ) as u128,
+            0
+        ),
+    )];
+    if flag_dst_base_fp {
+        memory_values.push((
+            const_expr!((fp_value as i16 + offset_dst_val) as u32),
+            felt252_expr!("dst", dst as u128, 0),
+        ));
+    } else {
+        memory_values.push((
+            const_expr!((ap_value as i16 + offset_dst_val) as u32),
+            felt252_expr!("dst", dst as u128, 0),
+        ));
+    };
+    if flag_op0_base_fp {
+        memory_values.push((
+            const_expr!((fp_value as i16 + offset0_val) as u32),
+            felt252_expr!("op0", op0 as u128, 0),
+        ));
+    } else {
+        memory_values.push((
+            const_expr!((ap_value as i16 + offset0_val) as u32),
+            felt252_expr!("op0", op0 as u128, 0),
+        ));
+    }
+    if flag_op1_imm {
+        memory_values.push((
+            const_expr!(pc_value + 1),
+            felt252_expr!("op1", op1 as u128, 0),
+        ));
+    } else if flag_op1_base_fp {
+        memory_values.push((
+            const_expr!((fp_value as i16 + offset1_val) as u32),
+            felt252_expr!("op1", op1 as u128, 0),
+        ));
+    } else {
+        memory_values.push((
+            const_expr!((ap_value as i16 + offset1_val) as u32),
+            felt252_expr!("op1", op1 as u128, 0),
+        ));
+    };
+    mul_small_opcode.memory = Felt252IdMemory::new_with_data(memory_values);
+
+    // Run air function
+
+    let registry = AirFnRegistry::new(&mul_small_opcode);
+    let (state, [next_pc, next_ap, next_fp]) =
+        registry.run_air(&mul_small_opcode, [pc.clone(), ap.clone(), fp.clone()]);
+
+    // Check output
+    assert_eq!(next_fp.calc(), fp.calc());
+    if flag_ap_update_add_1 {
+        assert_eq!(next_ap.calc(), (ap_value + 1).to_string());
+    } else {
+        assert_eq!(next_ap.calc(), ap.calc());
+    }
+    if flag_op1_imm {
+        assert_eq!(next_pc.calc(), (pc_value + 2).to_string());
+    } else {
+        assert_eq!(next_pc.calc(), (pc_value + 1).to_string());
+    };
+
+    assert_eq!(
+        state.calc(),
+        expected_state
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+    );
+
+    if let Some(expected_air_body) = expected_air_body {
+        let entry = registry.get_air_fn_entry(&mul_small_opcode.name());
+        assert_eq!(
+            entry
+                .air_body
+                .iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>(),
+            expected_air_body
+        );
+    }
+}
+
+#[test]
+fn test_mul_small_not_imm() {
+    test_mul_small(
+        [true, false, false, false, true, false],
+        [3, 5, 7],
+        1042584088,
+        32123,
+        32456,
+        Some(&[
+            "tmp_0 = [\
+                MulSmallOpcode_is_immediate_false_input[0], \
+                MulSmallOpcode_is_immediate_false_input[1], \
+                MulSmallOpcode_is_immediate_false_input[2]]",
+            "Deduction: tmp_0[0]",
+            "Deduction: tmp_0[1]",
+            "Deduction: tmp_0[2]",
+            "(\
+                [\
+                    (((state[3] + (state[4] * const_512)) + const_0) - const_32768), \
+                    (((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768), \
+                    (((state[8] + (state[9] * const_16)) + (state[10] * const_8192)) - const_32768)\
+                ], [\
+                    Bool::from_m31(state[11]), \
+                    Bool::from_m31(state[12]), \
+                    const_false, Bool::from_m31(state[13]), \
+                    Bool::from_m31(state[14]), \
+                    const_false, \
+                    const_true, \
+                    const_false, \
+                    const_false, \
+                    const_false, \
+                    const_false, \
+                    Bool::from_m31(state[15]), \
+                    const_false, const_false, \
+                    const_true\
+                ]\
+            ) = DecodeInstruction_385376571fef8ea3(state[0])",
+            "Felt252::from_limbs(zero_extend([state[18], state[19], state[20], state[21]])) = \
+                ReadPositive_num_bits_30((\
+                    ((state[11] * state[2]) + ((const_1 - state[11]) * state[1])) + \
+                    (((state[3] + (state[4] * const_512)) + const_0) - const_32768)\
+                ))",
+            "Felt252::from_limbs(zero_extend([state[23], state[24]])) = \
+                ReadPositive_num_bits_15((\
+                    ((state[12] * state[2]) + ((const_1 - state[12]) * state[1])) + \
+                    (((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768)\
+                ))",
+            "Constraint: ((state[13] + state[14]) - const_1)",
+            "Felt252::from_limbs(zero_extend([state[26], state[27]])) = \
+                ReadPositive_num_bits_15((\
+                    ((state[13] * state[2]) + (state[14] * state[1])) + \
+                    (((state[8] + (state[9] * const_16)) + (state[10] * const_8192)) - const_32768)\
+                ))",
+            "Constraint: (\
+                (((state[18] + (const_512 * state[19])) + (const_262144 * state[20])) + (const_134217728 * state[21])) - \
+                ((state[23] + (const_512 * state[24])) * (state[26] + (const_512 * state[27]))))"
+        ]),
+        vec![
+            10, 50, 100, 3, 64, 1, 1, 16, 7, 0, 4, 1, 0, 0, 1, 0, 0, 1, 24, 73, 393, 7, 2, 379, 62, 3, 200, 63]
+    );
+}
+
+#[test]
+#[should_panic(expected = "Added incorrect constraint (does not evalutate to 0)")]
+fn test_mul_mod_not_equal() {
+    test_mul_small(
+        [false, true, true, false, false, true],
+        [3, 5, 7],
+        1042584088,
+        32123,
+        32457,
+        None,
+        vec![],
+    );
+}
+
+#[test]
+#[should_panic(expected = "RangeCheck failed on element 0: RangeCheck6 on input 64")]
+fn test_mul_mod_over_15bit() {
+    test_mul_small(
+        [false, true, true, false, false, true],
+        [3, 5, 7],
+        32768,
+        32768,
+        1,
+        None,
+        vec![],
+    );
+}
+
+#[test]
+fn test_mul_small_imm() {
+    test_mul_small(
+        [true, false, true, false, true, false],
+        [-3, -5, 1],
+        56,
+        7,
+        8,
+        Some(&[
+            "tmp_0 = [\
+                MulSmallOpcode_is_immediate_true_input[0], \
+                MulSmallOpcode_is_immediate_true_input[1], \
+                MulSmallOpcode_is_immediate_true_input[2]\
+                ]",
+            "Deduction: tmp_0[0]",
+            "Deduction: tmp_0[1]",
+            "Deduction: tmp_0[2]",
+            "(\
+                [\
+                    (((state[3] + (state[4] * const_512)) + const_0) - const_32768), \
+                    (((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768), \
+                    const_1\
+                ], [\
+                    Bool::from_m31(state[8]), \
+                    Bool::from_m31(state[9]), \
+                    const_true, \
+                    const_false, \
+                    const_false, \
+                    const_false, \
+                    const_true, \
+                    const_false, \
+                    const_false, \
+                    const_false, \
+                    const_false, \
+                    Bool::from_m31(state[10]), \
+                    const_false, \
+                    const_false, \
+                    const_true\
+                ]\
+            ) = DecodeInstruction_22fdd1d4be52f69d(state[0])",
+            "Felt252::from_limbs(zero_extend([state[13], state[14], state[15], state[16]])) = \
+                ReadPositive_num_bits_30((\
+                    ((state[8] * state[2]) + ((const_1 - state[8]) * state[1])) + \
+                    (((state[3] + (state[4] * const_512)) + const_0) - const_32768)\
+                ))",
+            "Felt252::from_limbs(zero_extend([state[18], state[19]])) = \
+                ReadPositive_num_bits_15((\
+                    ((state[9] * state[2]) + ((const_1 - state[9]) * state[1])) + \
+                    (((state[5] + (state[6] * const_4)) + (state[7] * const_2048)) - const_32768)\
+                ))",
+            "Felt252::from_limbs(zero_extend([state[21], state[22]])) = \
+                ReadPositive_num_bits_15((state[0] + const_1))",
+            "Constraint: (\
+                (((state[13] + (const_512 * state[14])) + (const_262144 * state[15])) + (const_134217728 * state[16])) - \
+                ((state[18] + (const_512 * state[19])) * (state[21] + (const_512 * state[22]))))"
+        ]),
+        vec![
+            10, 50, 100, 509, 63, 3, 510, 15, 1, 0, 0, 0, 1, 56, 0, 0, 0, 2, 7, 0, 3, 8, 0],
+    );
+}
