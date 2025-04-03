c97dfbef32b80d45a0ad1f14fd917e8b6ff09f0e
anatg <anatg@starkware.co>
2025-04-03 17:01:25 +0300

    Forgot to add new components to mod.rs

---
+++ crates/code_gen_regression/cairo_air/src/components/range_check_3_6_6_3.rs
@@ -0,0 +1,67 @@
+#![allow(non_camel_case_types)]
+#![allow(unused_imports)]
+use num_traits::One;
+use stwo_prover::constraint_framework::logup::{LogupAtRow, LookupElements};
+use stwo_prover::constraint_framework::{EvalAtRow, FrameworkComponent, FrameworkEval};
+use stwo_prover::core::backend::simd::m31::PackedM31;
+use stwo_prover::core::channel::Channel;
+use stwo_prover::core::fields::m31::M31;
+use stwo_prover::core::fields::qm31::SecureField;
+use stwo_prover::core::fields::secure_column::SECURE_EXTENSION_DEGREE;
+use stwo_prover::core::pcs::TreeVec;
+use stwo_prover::relation;
+
+use crate::components::range_check_3_6_6_3;
+
+relation!(RelationElements, 4);
+pub struct RangeCheck_3_6_6_3Eval {
+    pub claim: Claim,
+    pub interaction_claim: InteractionClaim,
+    pub range_check_3_6_6_3_lookup_elements: range_check_3_6_6_3::RelationElements,
+}
+
+#[derive(Copy, Clone)]
+pub struct Claim {
+    pub log_size: u32,
+    pub n_calls: usize,
+}
+impl Claim {
+    pub fn log_sizes(&self) -> TreeVec<Vec<u32>> {
+        let interaction_0_log_sizes = vec![self.log_size; 0];
+        let interaction_1_log_sizes = vec![self.log_size; SECURE_EXTENSION_DEGREE * 4];
+        TreeVec::new(vec![interaction_0_log_sizes, interaction_1_log_sizes])
+    }
+
+    pub fn mix_into(&self, channel: &mut impl Channel) {
+        channel.mix_u64(self.log_size as u64);
+        channel.mix_u64(self.n_calls as u64);
+    }
+}
+
+pub struct InteractionClaim {
+    pub claimed_sum: SecureField,
+}
+impl InteractionClaim {
+    pub fn mix_into(&self, channel: &mut impl Channel) {
+        channel.mix_felts(&[self.claimed_sum]);
+    }
+}
+
+#[allow(non_snake_case)]
+pub type RangeCheck_3_6_6_3Component = FrameworkComponent<RangeCheck_3_6_6_3Eval>;
+
+impl FrameworkEval for RangeCheck_3_6_6_3Eval {
+    fn log_size(&self) -> u32 {
+        self.claim.log_size
+    }
+
+    fn max_constraint_log_degree_bound(&self) -> u32 {
+        self.log_size() + 1
+    }
+
+    #[allow(unused_parens)]
+    #[allow(clippy::double_parens)]
+    fn evaluate<E: EvalAtRow>(&self, mut _eval: E) -> E {
+        todo!()
+    }
+}
