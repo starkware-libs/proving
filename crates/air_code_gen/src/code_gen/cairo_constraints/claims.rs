2be54aa25f3e8a1d0e511519b6ee7504eea4951f
anatg <anatg@starkware.co>
2025-05-21 12:47:23 +0300

    Add claims and parse constraints

---
+++ crates/air_code_gen/src/code_gen/cairo_constraints/claims.rs
@@ -0,0 +1,75 @@
+use compiled_casm_air::compiled_structs::CompiledAirFn;
+use genco::lang::rust;
+use genco::quote;
+
+use super::utils::{get_log_size, is_const_size_component, n_logup_columns};
+
+pub fn gen_claim_struct(air_fn: &CompiledAirFn) -> rust::Tokens {
+    let mut code = rust::Tokens::new();
+    code.append(quote! {
+        #[derive(Drop, Serde, Copy)]
+        pub struct Claim {
+            $(get_claim_members(air_fn))
+        }
+
+        #[generate_trait]
+        pub impl ClaimImpl of ClaimTrait {
+            fn log_sizes(self: @Claim) -> TreeArray<Span<u32>> {
+                let log_size = $(get_log_size(air_fn, true));
+                let preprocessed_log_sizes = array![log_size].span();
+                let trace_log_sizes = ArrayImpl::new_repeated(N_TRACE_COLUMNS, log_size).span();
+                let interaction_log_sizes = ArrayImpl::new_repeated($(n_logup_columns(air_fn)), log_size).span();
+                array![preprocessed_log_sizes, trace_log_sizes, interaction_log_sizes]
+            }
+
+            fn mix_into(self: @Claim, ref channel: Channel) {
+                $(gen_mix_into(air_fn))
+            }
+        }
+    });
+    code
+}
+
+fn get_claim_members(air_fn: &CompiledAirFn) -> rust::Tokens {
+    let mut members = rust::Tokens::new();
+    if !is_const_size_component(air_fn) {
+        members.append(quote! { pub log_size: u32, });
+    };
+    for public_param in &air_fn.public_params {
+        members.append(quote! {
+            pub $(public_param.name()): u32,
+        });
+    }
+    members
+}
+
+fn gen_mix_into(air_fn: &CompiledAirFn) -> rust::Tokens {
+    let mut code = rust::Tokens::new();
+    if !is_const_size_component(air_fn) {
+        code.append(quote! {
+            channel.mix_u64(($(get_log_size(air_fn, true))).into());
+        });
+    }
+    for param in &air_fn.public_params {
+        code.append(quote! {
+            channel.mix_u64((*self.$(param.name())).into());
+        });
+    }
+    code
+}
+
+pub fn gen_interaction_claim_struct() -> rust::Tokens {
+    quote! {
+        #[derive(Drop, Serde, Copy)]
+        pub struct InteractionClaim {
+            pub claimed_sum: QM31,
+        }
+
+        #[generate_trait]
+        pub impl InteractionClaimImpl of InteractionClaimTrait {
+            fn mix_into(self: @InteractionClaim, ref channel: Channel) {
+                channel.mix_felts([*self.claimed_sum].span());
+            }
+        }
+    }
+}
--- crates/air_code_gen/src/code_gen/cairo_constraints/mod.rs
+++ crates/air_code_gen/src/code_gen/cairo_constraints/mod.rs
@@ -1,2 +1,3 @@
+pub mod claims;
 pub mod component;
 pub mod utils;
