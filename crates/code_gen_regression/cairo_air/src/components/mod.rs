5da0174fa018c1cd366aedc8d8ee28f96cc3a422
anatg <anatg@starkware.co>
2025-12-29 10:52:05 +0200

    Fix autogened code

--- crates/code_gen_regression/cairo_air/src/components/mod.rs
+++ crates/code_gen_regression/cairo_air/src/components/mod.rs
@@ -1,17 +1,11 @@
 pub mod mul_mod_builtin;
-<<<<<<< HEAD
-pub mod partial_ec_mul_bits_per_window_18;
-pub mod pedersen_points_table_bits_per_window_18;
 pub mod range_check_builtin;
-=======
 pub mod partial_ec_mul_window_bits_18;
 pub mod pedersen_points_table_window_bits_18;
-pub mod range_check_builtin_bits_128;
->>>>>>> 970e8f9d (Change bits per window to window bits)
 pub mod range_check_20;
+pub mod range_check_9_9;
 pub mod range_check_7_2_5;
 pub mod triple_xor_32;
 pub mod verify_bitwise_xor_8;
-pub mod jnz_opcode_taken;
 pub mod verify_instruction;
-pub mod range_check_9_9;
+pub mod jnz_opcode_taken;
