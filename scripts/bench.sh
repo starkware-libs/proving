0a9f1d01e191c3d1539217125d12e34f08bd7b32
leo-starkware <leo@starkware.co>
2026-07-23 10:35:17 +0300

    Monorepo setup: specify stwo package in scripts/bench.sh.

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -4,4 +4,4 @@
 # Can be used as a drop in replacement for `cargo bench`.
 # For example, `./scripts/bench.sh` will run all benchmarks.
 # or `./scripts/bench.sh M31` will run only the M31 benchmarks.
-RUSTFLAGS="-Awarnings -C target-cpu=native -C opt-level=3" cargo criterion --features prover --output-format bencher $@
+RUSTFLAGS="-Awarnings -C target-cpu=native -C opt-level=3" cargo criterion -p stwo --features prover --output-format bencher $@
